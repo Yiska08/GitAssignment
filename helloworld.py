95a863c0b1d399752f01955c4215350a93113916
Yiska08 <graciellayiska08@gmail.com>
2025-07-06 21:01:02 -0700

    add mean

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,12 @@
 
 
 print("Hello, bubba world!")
+
+def mean(input):
+  sum = 0
+  for entry in input:
+    sum += entry
+  return sum / len(input)
+
+my_list = [0, 1, 2, 3, 4, 5]
+print(mean(my_list))
