45fe8254a22d43a7a26ffa56b75ffa3a7712af44
Yiska08 <graciellayiska08@gmail.com>
2025-07-06 21:03:14 -0700

    added median function

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,21 @@
-
+import math
 
 print("Hello, bubba world!")
+
+#Assuming that the list is already in ascending order
+
+def median(input):
+  input = sorted(input)
+  medianVal = 0
+  if len(input) % 2 == 0:
+    a = input[math.floor(len(input) / 2 - 1) ]
+    b = input[math.floor(len(input) / 2)]
+    medianVal = (a + b) / 2
+  else:
+    medianVal = input[math.floor(len(input) / 2)]
+  return medianVal
+
+
+
+print(median([0, 3, 5, 7, 10, 11, 32, 87, 100, 999]))
+
